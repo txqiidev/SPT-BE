3a04b0ccbcd10942bcfe254ef9af05cdea511293
txqiidev <timothy.triebold@gmail.com>
2020-07-26 16:52:06 +0200

    ficed alles wasi kaputt gmacht ha

--- routes/modulesAdmin.js
+++ routes/modulesAdmin.js
@@ -1,96 +1,96 @@
-const express = require("express");
-const adminDB = require("../db/adminDB");
-const { validate } = require("../services/validation/admin/module");
-const auth = require("../middleware/authenticator");
-const admin = require("../middleware/admin");
+const express = require('express')
+const adminDB = require('../db/adminDB')
+const { validate } = require('../services/validation/admin/module')
+const auth = require('../middleware/authenticator')
+const admin = require('../middleware/admin')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/", async (req, res) => {
+router.get('/all/:id', async (req, res) => {
   try {
-    let results = await adminDB.modulesAll();
-    res.json(results);
+    let results = await adminDB.modulesAll(req.params.id)
+    res.json(results)
   } catch (error) {
-    res.status(error.response.status);
-    return res.send(error.message);
+    res.status(error.response.status)
+    return res.send(error.message)
   }
-});
+})
 
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
-    let results = await adminDB.moduleOne(req.params.id);
-    res.json(results);
+    let results = await adminDB.moduleOne(req.params.id)
+    res.json(results)
   } catch (error) {
-    res.status(error.response.status);
-    return res.send(error.message);
+    res.status(error.response.status)
+    return res.send(error.message)
   }
-});
+})
 
-router.get("/PrerequisiteModules/:id", async (req, res) => {
+router.get('/PrerequisiteModules/:id', async (req, res) => {
   try {
-    let results = await adminDB.prerequisiteModules(req.params.id);
-    res.json(results);
+    let results = await adminDB.prerequisiteModules(req.params.id)
+    res.json(results)
   } catch (error) {
-    res.status(error.response.status);
-    return res.send(error.message);
+    res.status(error.response.status)
+    return res.send(error.message)
   }
-});
+})
 
-router.put("/URL", [auth, admin], async (req, res) => {
-  const { error } = validate.validateURL(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.put('/URL', [auth, admin], async (req, res) => {
+  const { error } = validate.validateURL(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
   try {
-    let results = await adminDB.moduleUpdateURL(req.body.id, req.body.url);
-    res.json(results);
+    let results = await adminDB.moduleUpdateURL(req.body.id, req.body.url)
+    res.json(results)
   } catch (error) {
-    res.status(error.response.status);
-    return res.send(error.message);
+    res.status(error.response.status)
+    return res.send(error.message)
   }
-});
+})
 
-router.put("/HasPrerequisite", [auth, admin], async (req, res) => {
-  const { error } = validate.validateHasPrerequisite(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.put('/HasPrerequisite', [auth, admin], async (req, res) => {
+  const { error } = validate.validateHasPrerequisite(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
   try {
     let results = await adminDB.moduleUpdateHasPrerequisite(
       req.body.id,
       req.body.hasPrerequisite
-    );
-    res.json(results);
+    )
+    res.json(results)
   } catch (error) {
-    res.status(error.response.status);
-    return res.send(error.message);
+    res.status(error.response.status)
+    return res.send(error.message)
   }
-});
+})
 
-router.post("/Prerequisite", [auth, admin], async (req, res) => {
-  const { error } = validate.validatePrerequisite(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post('/Prerequisite', [auth, admin], async (req, res) => {
+  const { error } = validate.validatePrerequisite(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
   try {
     let results = await adminDB.moduleAddPrerequisite(
       req.body.id,
       req.body.idPrerequisite
-    );
-    res.json(results);
+    )
+    res.json(results)
   } catch (error) {
-    res.status(error.response.status);
-    return res.send(error.message);
+    res.status(error.response.status)
+    return res.send(error.message)
   }
-});
+})
 
-router.delete("/Prerequisite", [auth, admin], async (req, res) => {
-  const { error } = validate.validatePrerequisite(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.delete('/Prerequisite', [auth, admin], async (req, res) => {
+  const { error } = validate.validatePrerequisite(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
   try {
     let results = await adminDB.deletePrerequisite(
       req.body.id,
       req.body.idPrerequisite
-    );
-    res.json(results);
+    )
+    res.json(results)
   } catch (error) {
-    res.status(error.response.status);
-    return res.send(error.message);
+    res.status(error.response.status)
+    return res.send(error.message)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
